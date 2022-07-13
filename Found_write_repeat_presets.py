87ad1d6dbe0211fe56ea3869b837fa48b1472bcd
Stepan <stpokorn@ya.ru>
2022-07-13 22:58:50 +0300

    нахожу все нужные записи + дочерние

--- Found_write_repeat_presets.py
+++ Found_write_repeat_presets.py
@@ -1,9 +1,7 @@
-from optparse import Values
-
 
 def found_presets():
     stroks = []
-    with open('result.csv', newline='') as f:
+    with open('test.csv', newline='') as f:
             reader = f.readlines()
             for stringg in reader:
                 if len(stringg.split('|')[1]):
@@ -18,19 +16,20 @@ result = {}
 for line in file:
     result.setdefault(line.split('|')[1],[]).append(line)
 
-lopes = [elem for elem, count in result.items() if len(count) > 1]
 result = {elem: count for elem, count in result.items() if len(count) > 1}
 
-pok = []
+trol = []
 for key, value in result.items():
     for i in value:
-        pok.append(i)
+        if i in trol:
+            break
+        else:
+            trol.append(i)
 
 
-def write_file(pok):        # Собирает файл из списка.
-    f = open( 'итоги_полный_файл.txt', 'w' )
-    f.write("".join(pok))
+def write_file(trol):        # Собирает файл из списка.
+    f = open( 'trol.txt', 'w' )
+    f.write("".join(trol))
     f.close
     
-write_file(pok)
-        
+write_file(trol)
